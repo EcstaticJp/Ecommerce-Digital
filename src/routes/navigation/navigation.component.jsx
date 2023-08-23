62658ef090dbb07c6ab748170fda35f0fbf0d84c
Jyoti PAtnaik <jyotipatnaik4@gmail.com>
2023-08-23 14:41:47 +0530

    Navigation bar and gitignore file changes

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,37 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+import {ReactComponent as CrwnLogo } from '../../assets/crown.svg'
+import './navigation.styles.scss'
+import { ReactComponent as CartIcon } from '../../assets/cart.svg'
+
+const Navigation = () => {
+  return (
+    <Fragment>
+        <div className="navigation">
+            <Link className="logo-container" to='/'>
+                <CrwnLogo className="logo"/>
+            </Link>
+            
+            <div className="nav-links-container">
+                <Link className="nav-link" to='/shop'>
+                    Shop
+                </Link>
+                <Link className="nav-link" to='/shop'>
+                    Contact
+                </Link>
+                <Link className="nav-link" to='/shop'>
+                    Sign in
+                </Link>
+                <Link className="nav-link" to='/shop'>
+                    <CartIcon className="cart-logo"/>
+                </Link>
+            </div>
+        </div>
+
+        <Outlet />
+      
+    </Fragment>
+  );
+};
+
+export default Navigation;
